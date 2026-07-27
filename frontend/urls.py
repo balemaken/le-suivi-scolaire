d0e3320a7da34771ea23ec40ed4915fd9613f382
balemaken <bricebalemaken960@gmail.com>
2026-07-27 18:55:35 +0100

    création des 1er interfaces

---
+++ frontend/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.pageAcceuil, name='pageAcceuil'),
+    path('login/', views.login, name='login'),
+    path('incription/',views.inscription,name='inscription')
+]
---
+++ frontend/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def pageAcceuil(request):
+    return render(request,'frontend/pageAcceuil.html')
+
+
+def login(request):
+    return render(request,'frontend/login.html')
+
+def inscription(request):
+    return render(request,'frontend/inscription.html')
+    
+
